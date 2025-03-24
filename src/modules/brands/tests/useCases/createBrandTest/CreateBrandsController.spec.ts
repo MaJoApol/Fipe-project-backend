08e8372a4993e615609c4268bcb8b3a831d77bbd
MariaJo <mariajoanaceron@gmail.com>
2025-03-24 16:59:24 -0300

    create Brand Controller not working

--- src/modules/brands/tests/useCases/createBrandTest/CreateBrandsController.spec.ts
+++ src/modules/brands/tests/useCases/createBrandTest/CreateBrandsController.spec.ts
@@ -1,42 +1,59 @@
 
 import "reflect-metadata";
 import { describe } from "node:test";
-import { headers } from "next/headers";
 import { container } from "tsyringe";
+import { CreateBrandUseCase } from "../../../useCases/createBrands/CreateBrandUseCase";
+import { Request } from "express";
+import { BrandDTO } from "../../../dtos/BrandDTO";
+import { CreateBrandController } from "../../../useCases/createBrands/CreateBrandController";
 
 describe("Create Brands Controller", () => {
-    let createBrandUseCaseMock;
-    let createBrandController;
-    let request;
-    let response;
+    let createBrandUseCaseMock: jest.Mocked<CreateBrandUseCase>;
+    let createBrandController: CreateBrandController;
+    let request: Request;
+    let response: Response;
 
     beforeEach(()=>{
         createBrandUseCaseMock = {
             execute: jest.fn()
-        };
+        } as unknown as jest.Mocked<CreateBrandUseCase>
 
         jest.spyOn(container, "resolve").mockReturnValue(createBrandUseCaseMock);
+
+        createBrandController = new CreateBrandController();
 
         request = {
             body: {name: "Teste"},
             headers: { authorization: "token123"}
-        }
+        } as jest.Mocked<Request>;
 
         response = {
-            status: jest.fn(),
+            status: jest.fn().mockReturnThis(),
             json: jest.fn()
+        } as unknown as jest.Mocked<Response>;
+    });
+
+        const mockedData: BrandDTO = {
+            name: "TesteName",
+            id: "1",
+            fipeCode: null,
+            isDeleted: false,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            deletedAt: new Date(),
+            createdById: "valid-token",
+            updatedById: null,
+            deletedById: null
         }
+    
 
+    it("Deve chamar o Use Case e retornar 201", async () => {
+        createBrandUseCaseMock.execute.mockResolvedValue(mockedData);
+        await createBrandController.handle(request, response);
 
-
-        
-
-
+        expect(createBrandUseCaseMock.execute).toHaveBeenCalledWith(request.body, "token123");
+        expect(response.status).toHaveBeenCalledWith(201);
+        expect(response.json).toHaveBeenCalledWith({message: "Criado com sucesso!"})
     })
-
-
-
-
-
 
 })
