033f6049650950ce9cd2934fd4aebe8d12f2daef
MajoApol <mariajoanaceorn@gmail.com>
2025-03-29 21:31:23 -0300

    .

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export default {
     secret_token: process.env.SECRET_TOKEN || "",
     secret_refresh_token: process.env.SECRET_REFRESH_TOKEN || "",
-    expires_in_token: "15",
+    expires_in_token: "15m",
     expires_in_token_minutes: 15,
     expires_in_refresh_token: "7200m",
     expires_refresh_token_minutes: 7200
